80d542a539b1dd8fd83964fef45e5d700cef03ae
arjun258 <66076537+arjun258@users.noreply.github.com>
2020-10-06 21:28:50 +0530

    project with errors

--- sketch.js
+++ sketch.js
@@ -10,6 +10,16 @@ function setup() {
   weight= random(400,1500);
 
   car.velocityX=speed;
+  
+  
+
+}
+
+function draw() {
+  background(255,255,255);  
+  wall.shapeColor=color("blue");
+  car.collide(wall);
+
   if(wall.x-car.x<(car.width+wall.width)/2)
   {
     car.velocityX=0;
@@ -26,16 +36,6 @@ function setup() {
     }
   
   }
-  
-
-}
-
-function draw() {
-  background(255,255,255);  
-  wall.shapeColor=color("blue");
-  car.collide(wall);
-
-
 
 
 
