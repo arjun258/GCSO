20d59804a903bea26ebe03614a7692cff16a6f77
arjun258 <66076537+arjun258@users.noreply.github.com>
2020-10-08 21:18:21 +0530

    Project Completed

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function draw() {
   wall.shapeColor=color("blue");
   car.collide(wall);
 
-  if(wall.x-car.x<(car.width+wall.width)/2)
+  if(wall.x-car.x<=(car.width+wall.width)/2)
   {
     car.velocityX=0;
     var deformation=0.5*weight*speed*speed/22509;
